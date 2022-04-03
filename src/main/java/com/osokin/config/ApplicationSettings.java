e731d7ee771579a439e1e3ea83e572c0689bfa05
egoone2 <arikosokin2822@gmail.com>
2022-04-03 22:38:34 +0700

    add filePath feature

--- src/main/java/com/osokin/config/ApplicationSettings.java
+++ src/main/java/com/osokin/config/ApplicationSettings.java
@@ -1,10 +1,7 @@
 package com.osokin.config;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class ApplicationSettings {
-    public static final String FILE_PATH = "C:\\Users\\whale\\IdeaProjects\\ava111\\src\\main\\resources\\source.txt";
+    public static final String FILE_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\source.txt";
     public static final String DATE_PATTERN = "dd/MM/yy";
-    public static final String OUTPUT_FILE_PATH = "output.txt"; //TODO classpath
+    public static final String OUTPUT_FILE_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\output.txt";
 }
